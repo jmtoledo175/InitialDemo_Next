86979305da9e640b862b143cebaf995ae6a2842b
jmtoledo175 <juli.toledo175@gmail.com>
2023-11-13 19:45:24 -0300

    first commit

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,30 +1,23 @@
 import Head from "next/head";
-import Link from "next/link";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import styles from "../styles/Home.module.css";
+import Link from "next/link"
 export default function Home() {
   return (
     <>
       <Head>
-        <title>home</title>
+        <title>About</title>
         <meta name="description" content="Home page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a{" "}
-          <Link href="/about">
-            About Us
-          </Link>
+          Ir a <Link href="/">home</Link>
         </h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            <code className={styles.code}>pages/about.tsx</code>
           </p>
         </div>
         <div className={styles.grid}></div>
